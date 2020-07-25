6b784809239df9eac79863157702e09c262f748b
Roman Serikoff <serikoff@li.ru>
2020-07-25 18:50:43 +0300

    v 1.0.6

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const type = (value, compareType) => {
-	const type = Object.prototype.toString.call(value).replace(/\[object\s|\]/g, '');
-	if (compareType) return compareType === type;
-	return type;
+	const _type = Object.prototype.toString.call(value).replace(/\[object\s|\]/g, '');
+	if (compareType) {
+		if (type(compareType) === 'Function') compareType = type(compareType.prototype);
+		return compareType === _type;
+	}
+	return _type;
 };
 
 const has = (obj, path) => {
 	if (typeof path === 'string') path = path.split('.');
 	if (path.length === 1) {
-        return (obj && obj.hasOwnProperty(path[0]));
+		return obj && obj.hasOwnProperty(path[0]);
 	} else {
 		return has((obj && obj[path[0]]) || {}, path.slice(1));
 	}
@@ -38,31 +41,41 @@ const set = (obj = {}, path, value) => {
 			return true;
 		} else return false;
 	} else {
-        if (!['Object', 'Array'].includes(type(obj[path[0]]))) obj[path[0]] = {};
-        return set(obj[path[0]], path.slice(1), value);
+		if (!['Object', 'Array'].includes(type(obj[path[0]]))) obj[path[0]] = {};
+		return set(obj[path[0]], path.slice(1), value);
 	}
 };
 
-const findProps = (obj, prop, path, searhArray = true) => {
+const findProps = (obj, prop, { path: deepPath, deep, includes, searhArray = true }) => {
 	const result = [];
 	const find = (obj, path) => {
+		if (
+			type(deep, 'Number') &&
+			((deepPath && deepPath.split('.').length) || 0) + deep < path.length
+		)
+			return;
 		for (const key in obj) {
 			if (key == prop) {
 				result.push({
 					path: [...path, `${key}`].join('.'),
 					value: obj[key],
 				});
+			} else if (includes) {
+				if (key.search(type(includes, 'RegExp') ? includes : new RegExp(`${prop}`, 'g')) > -1) {
+					result.push({
+						path: [...path, `${key}`].join('.'),
+						value: obj[key],
+					});
+				}
 			}
 			if (type(obj[key], 'Object')) find(obj[key], [...path, `${key}`]);
 			else if (type(obj[key], 'Array') && searhArray) find(obj[key], [...path, `${key}`]);
 		}
-    };
-    if (path) {
-		const newObj = get(obj, path);
-		find(newObj, [path]);
-	} else {
-        find(obj, []);
-    }
+	};
+	if (deepPath) {
+		const newObj = get(obj, deepPath);
+		find(newObj, [deepPath]);
+	} else find(obj, []);
 	return result;
 };
 
@@ -77,11 +90,11 @@ const findProp = (obj, prop, path, resultPath = []) => {
 			value: obj[prop],
 		};
 	for (const key in obj) {
-        if (type(obj[key], 'Object')) {
-            const found = findProp(obj[key], prop, false, [...resultPath, `${key}`]);
-            if (found) return found;
-        }
+		if (type(obj[key], 'Object')) {
+			const found = findProp(obj[key], prop, false, [...resultPath, `${key}`]);
+			if (found) return found;
+		}
 	}
 };
 
-module.export = { findProp, findProps, set, get, has, type };
+module.exports = { findProp, findProps, set, get, has, type };
